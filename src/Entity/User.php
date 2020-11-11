0f795fdeccfcc2fd104dc66d8438c4c3bf0d83be
nadir958 <layoune95@gmail.com>
2020-11-11 14:57:39 +0100

    authentication jwt

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,14 +5,18 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ApiResource
+ * @ApiResource(
+ *  normalizationContext={
+ *      "groups"={"users_read"}
+ * })
  * @UniqueEntity("email",message="un utilisateur ayant cette adresse email existe déja")
  */
 class User implements UserInterface
@@ -21,11 +25,13 @@ class User implements UserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"users_read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"users_read"})
      * @Assert\NotBlank(message="email doit etre renseigné!")
      * @Assert\Email(message="email doit etre valide")
 
@@ -34,6 +40,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="json")
+     * @Groups({"users_read"})
      */
     private $roles = [];
 
